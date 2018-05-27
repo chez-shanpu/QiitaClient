10263691e488e6ecdc8cff7baa84d7c4adb01123
shanpu <cheztomo513@gmail.com>
2018-05-27 21:30:21 +0900

    記事ビュー完成

--- app/src/main/java/sample/qiitaclient/MainActivity.kt
+++ app/src/main/java/sample/qiitaclient/MainActivity.kt
@@ -2,11 +2,25 @@ package sample.qiitaclient
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import sample.qiitaclient.model.Article
+import sample.qiitaclient.model.User
+import sample.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //ArticleViewのオブジェクト生成
+        val articleView = ArticleView(applicationContext)
+
+        //Articleオブジェクトを生成してArticleViewオブジェクトにセット
+        articleView.setArticle(Article(id = "123",
+                title = "test",
+                url = "http://www.example.com/articles/123",
+                user = User(id = "456", name = "shanpu", profileImageUrl = "")))
+
+        //このアクティビティにArticleViewオブジェクトをセット
+        setContentView(articleView)
     }
 }
